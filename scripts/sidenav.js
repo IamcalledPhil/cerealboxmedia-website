f8c114fd168a3146b73cc2ef5c8e5b9fba28922e
iamcalledphil <philippetercohn@gmail.com>
2020-05-03 17:58:56 +0200

    working

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,15 +1,15 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("screen-overlay").classList.remove("hidden");
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("screen-overlay").classList.add("hidden");
 }
 
-function showElement(elementToShow) {
-    document.getElementById(elementToShow).classList.toggle("visible");
+function toggleElement(elementToShow) {
+    document.getElementById(elementToShow).classList.toggle("hidden");
 }
